e5031b744e1e351de0587c97ad0cd60b09e52475
Aşkın <askinbolat@gmail.com>
2022-10-15 00:24:11 +0300

    algoritma soru 4

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace algoritma4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir cümle yazınız:");
+            string cümle = Console.ReadLine();
+
+            string[] kelime = cümle.Split();
+            Console.WriteLine(kelime.Length);
+
+            string bosluksuzcumle = cümle.Replace(" ","");
+            Console.WriteLine(bosluksuzcumle.Length);
+
+            
+
+        }
+    }
+}
